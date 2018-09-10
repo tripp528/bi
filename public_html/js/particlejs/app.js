8f540b160f67982a882c431864f136a1f148b1ca
tripp528 <tripp528@gmail.com>
2018-09-10 19:05:01 -0400

    more particle work

--- public_html/js/particlejs/app.js
+++ public_html/js/particlejs/app.js
@@ -11,128 +11,6 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-/*
-particlesJS('particles-js',
-
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 1000
-        }
-      },
-      "color": {
-        "value": "#003300"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 10,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 3
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#000000",
-        "opacity": 0,
-        "width": 10
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 200,
-          "line_linked": {
-            "opacity": 10,
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 100
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
-*/
 
 particlesJS("aa_particles", {
   "particles": {
@@ -195,7 +73,7 @@ particlesJS("aa_particles", {
       "random": false,
       "straight": false,
       "out_mode": "out",
-      "bounce": false,
+      "bounce": true,
       "attract": {
         "enable": false,
         "rotateX": 600,
@@ -208,7 +86,7 @@ particlesJS("aa_particles", {
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "grab"
+        "mode": "repulse"
       },
       "onclick": {
         "enable": true,
--- public_html/php/facility.php
+++ public_html/php/facility.php
@@ -1,20 +1,19 @@
-<?php //include 'php/header2.php'; ?>
 
 
-<div class="aa_sec center">
-  <div class="row">
-    <div class="col s12 l4">
-      <span class="aa_inner aa_txt count">11000</span> <br/>
-      <span class="aa_of aa_txt ">tons of CO_2</span>
-    </div>
-    <div class="col s12 l4">
-      <span class="aa_inner aa_txt count">174000000</span><br/>
-      <span class="aa_of aa_txt ">gallons of water</span>
-    </div>
-    <div class="col s12 l4">
-      <span class="aa_inner aa_txt count">21000</span><br/>
-      <span class="aa_of aa_txt ">MWh of energy</span>
-    </div>
+<div class="row aa_sec center">
+  <div class="col s4 aa_col">
+    <span class=" aa_txt aa_number count">11000</span> <br/>
+    <span class=" aa_txt aa_letter">tons of CO_2</span>
+  </div>
+
+  <div class="col s4 aa_col">
+    <span class="aa_txt aa_number count ">174000000</span><br/>
+    <span class=" aa_txt aa_letter">gallons of water</span>
+  </div>
+
+  <div class="col s4 aa_col">
+    <span class=" aa_txt aa_number count">21000</span><br/>
+    <span class=" aa_txt aa_letter">MWh of energy</span>
   </div>
 </div>
 <div id="aa_particles" class="z-depth-0"></div>
@@ -24,17 +23,36 @@
 
 
 
-<!--
-<div class="aa_sec center">
-  <span class="aa_of aa_txt count">100000</span>
-  <div class="aa_txt">
-    <span class="aa_inner">
-      Person <span class="aa_of">of</span> Person
-    </span>
+<!-- rows:
+<div class="row aa_sec center">
+  <div class="col s4 aa_col">
+    <div class="row aa_number no-margin">
+      <span class=" aa_txt count">11000</span> <br/>
+    </div>
+    <div class="row aa_letter no-margin">
+      <span class=" aa_txt ">tons of CO_2</span>
+    </div>
   </div>
 
+  <div class="col s4 aa_col">
+    <div class="row aa_number no-margin">
+      <span class="aa_txt count ">174000000</span><br/>
+    </div>
+    <div class="row aa_letter no-margin">
+      <span class=" aa_txt ">gallons of water</span>
+    </div>
+  </div>
+
+  <div class="col s4 aa_col">
+  <div class="row aa_number no-margin">
+    <span class=" aa_txt count">21000</span><br/>
+  </div>
+  <div class="row aa_letter no-margin">
+    <span class=" aa_txt ">MWh of energy</span>
+  </div>
+  </div>
 </div>
-<div id="aa_particles" class="z-depth-5"></div>
+<div id="aa_particles" class="z-depth-0"></div>
 -->
 
 <!-- particle scripts -->
